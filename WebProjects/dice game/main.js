548920d901fa79a22c630791458388c6f38e3d59
Lakshyaraj Singh Ranawat <lakshyaraj.singh.05@gmail.com>
2024-10-10 23:24:25 +0530

    final update

--- WebProjects/dice game/main.js
+++ WebProjects/dice game/main.js
@@ -1,22 +1,41 @@
 let image1 = document.querySelector(".dice1 img");
 let image2 = document.querySelector(".dice2 img");
-let num1=Math.floor(Math.random()*6+1);
-let newsrc1="images/"+num1+".jpg";
-image1.setAttribute("src",newsrc1);
-let num2=Math.floor(Math.random()*6+1);
-let newsrc2="images/"+num2+".jpg";
-image2.setAttribute("src",newsrc2);
-let el = document.createElement("div"); 
-el.classList.add("winningmessage");
-el.style.backgroundColor = "yellow";
-el.style.fontFamily = "cursive";
-el.style.fontSize = "50px";
-el.style.textAlign = "center";
-document.body.appendChild(el);
-if (num1 > num2) {
-    el.innerHTML = "Player 1 is the winner"; 
-} else if (num1 < num2) {
-    el.innerHTML = "Player 2 is the winner"; 
-} else {
-    el.innerHTML = "It is a draw"; 
+
+function Refresh(){
+    let num1=Math.floor(Math.random()*6+1);
+    let newsrc1="images/"+num1+".jpg";
+    image1.setAttribute("src",newsrc1);
+    let num2=Math.floor(Math.random()*6+1);
+    let newsrc2="images/"+num2+".jpg";
+    image2.setAttribute("src",newsrc2);
+   
+    win(num1,num2)
 }
+let el=document.querySelector(".result")
+function win(num1,num2){ 
+    el.classList.add("winningmessage");
+    
+    if (num1 > num2) {
+        
+        el.innerText = "Player 1 is the winner";
+       
+    } else if (num1 < num2) {
+       
+        el.innerText = "Player 2 is the winner"; 
+    } else {
+        
+        el.innerText= "It is a draw"; 
+        
+    }
+   
+}
+
+let btn=document.querySelector(".btn");
+btn.addEventListener("click",()=>{
+
+    el.innerText="";
+    Refresh();
+})
+
+
+
