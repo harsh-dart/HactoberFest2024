01fd92898f0c03d76ef16db4b357cfe2fff5085b
barath-007 <barath_23eca19@kgkite.ac.in>
2024-10-18 19:12:32 +0530

    Create prime.py

---
+++ C++/prime.py
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, i;
+    bool isPrime = true;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num <= 1) {
+        isPrime = false;
+    } else {
+        for (i = 2; i <= num / 2; ++i) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        cout << num << " is a prime number." << endl;
+    else
+        cout << num << " is not a prime number." << endl;
+
+    return 0;
+}
