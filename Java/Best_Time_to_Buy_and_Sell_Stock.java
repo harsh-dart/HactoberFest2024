8902b865b0b3d26149fe8ac282c25a0f94628f1e
ARVINDH-CT06 <24bct106@kgcas.com>
2024-10-18 21:03:44 +0530

    Update Best_Time_to_Buy_and_Sell_Stock.java

--- Java/Best_Time_to_Buy_and_Sell_Stock.java
+++ Java/Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,15 +1,23 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        // Return 0 if the prices array is empty
+        if (prices.length == 0) {
+            return 0;
+        }
+
         int maxProfit = 0;
-        int minValue = 100001;
-        for(int i=0;i<prices.length; i++){
-           if(minValue > prices[i]){
-               minValue = prices[i];
-           }
-            if(maxProfit < prices[i] - minValue){
-                maxProfit = prices[i] - minValue;
+        int minValue = prices[0]; // Initialize to the first price
+
+        for (int i = 1; i < prices.length; i++) {
+            // Update minValue if current price is lower
+            if (prices[i] < minValue) {
+                minValue = prices[i];
+            } else {
+                // Calculate potential profit and update maxProfit if it's higher
+                maxProfit = Math.max(maxProfit, prices[i] - minValue);
             }
         }
+        
         return maxProfit;
     }
 }
