bf3068362bf37a13f1f0dab425708e1bb3663d53
rajeshwariramya <103486631+rajeshwariramya@users.noreply.github.com>
2024-10-19 09:42:11 +0530

    Update nQueen.c

--- C/nQueen.c
+++ C/nQueen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// Function to print the chessboard solution
 void printSolution(int N, int board[N][N]) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++)
@@ -9,6 +10,7 @@ void printSolution(int N, int board[N][N]) {
     }
 }
 
+// Function to check if it's safe to place a queen
 bool isSafe(int N, int board[N][N], int row, int col) {
     // Check this row on the left side
     for (int i = 0; i < col; i++)
@@ -28,14 +30,16 @@ bool isSafe(int N, int board[N][N], int row, int col) {
     return true;
 }
 
+// Utility function to solve N-Queens problem using backtracking
 bool solveNQueensUtil(int N, int board[N][N], int col) {
     if (col >= N)
         return true;
 
     for (int i = 0; i < N; i++) {
         if (isSafe(N, board, i, col)) {
-            board[i][col] = 1;
+            board[i][col] = 1; // Place queen
 
+            // Recur to place the rest of the queens
             if (solveNQueensUtil(N, board, col + 1))
                 return true;
 
@@ -46,13 +50,16 @@ bool solveNQueensUtil(int N, int board[N][N], int col) {
     return false;
 }
 
+// Function to solve the N-Queens problem
 bool solveNQueens(int N) {
     int board[N][N];
 
+    // Initialize the board
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             board[i][j] = 0;
 
+    // Start solving the problem
     if (!solveNQueensUtil(N, board, 0)) {
         printf("Solution does not exist.\n");
         return false;
@@ -63,6 +70,7 @@ bool solveNQueens(int N) {
     return true;
 }
 
+// Main function to execute the program
 int main() {
     int N;
 
