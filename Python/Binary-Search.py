e35e4cddd135a66bcc35b41efe43dc37462a9fea
Mohd Aazam <padhan.azam98@gmail.com>
2024-10-23 10:22:21 +0530

    create function for Binary-Search.py

--- Python/Binary-Search.py
+++ Python/Binary-Search.py
@@ -1,20 +1,22 @@
+def binary_search(array, target):
+    left, right = 0, len(array) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1  # Indicates that the target was not found
+
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 target = 7
-left = 0
-right = len(array) - 1
-found = False
+result = binary_search(array, target)
 
-while left <= right:
-    mid = left + (right - left) // 2
-    if array[mid] == target:
-        found = True
-        break
-    elif array[mid] < target:
-        left = mid + 1
-    else:
-        right = mid - 1
-
-if found:
-    print("Element found at index:", mid)
+if result != -1:
+    print("Element found at index:", result)
 else:
     print("Element not found")
